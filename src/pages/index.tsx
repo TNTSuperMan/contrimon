fb32b53f110a080dd85ffe063dfd4ad6c3e3a406
TNTSuperMan <abworld@outlook.jp>
2025-04-13 00:07:52 +0900

    update button style target

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { CLIENT_ID } from "../../env";
 export default () => <>
     <h1>contrimon</h1>
     <span>より貢献したくなる。</span>
-    <button onClick={()=>location.href =
+    <button class="btn" onClick={()=>location.href =
         `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
     }>始める</button>
 </>;
