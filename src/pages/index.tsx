535b323bd9e81bc2e513226af9dc4bfaa3a7703e
TNTSuperMan <abworld@outlook.jp>
2025-04-13 00:12:23 +0900

    add oauth redirect target

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default () => <>
     <h1>contrimon</h1>
     <span>より貢献したくなる。</span>
     <button class="btn" onClick={()=>location.href =
-        `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+        `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${
+        encodeURIComponent(new URL("/oauth", location.origin).toString())}`
     }>始める</button>
 </>;
