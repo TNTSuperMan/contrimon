f118cfdc1c95ea2a11a8b82c8d8c1b2ea589477b
TNTSuperMan <abworld@outlook.jp>
2025-04-13 00:47:16 +0900

    add envs

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,10 @@
 import { Hono } from "hono";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: {
+    CLIENT_ID: string,
+    CLIENT_SECRET: string,
+    CLIENT: string,
+    SECRET: string
+}}>();
 
 export default app;
