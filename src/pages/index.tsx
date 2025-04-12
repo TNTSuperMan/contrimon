eb312ccf3b0b23e6397ae1e58951bad33ba36b0b
TNTSuperMan <abworld@outlook.jp>
2025-04-12 23:46:10 +0900

    add start button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { CLIENT_ID } from "../../env";
 export default () => <>
     <h1>contrimon</h1>
     <span>より貢献したくなる。</span>
-    <button class="start" onClick={()=>location.href =
+    <button onClick={()=>location.href =
         `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
     }>始める</button>
 </>;
