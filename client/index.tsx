9ddfb29397681e10668c42983d50d1d29c090ca8
TNTSuperMan <abworld@outlook.jp>
2025-04-13 18:31:20 +0900

    create home page

--- client/index.tsx
+++ client/index.tsx
@@ -4,11 +4,13 @@ import Router, { Route } from "preact-router";
 
 import index from "./pages/index";
 import oauth from "./pages/oauth";
+import home from "./pages/home";
 
 const main = () => 
     render(<Router>
         <Route path="/" component={index}/>
         <Route path="/oauth" component={oauth}/>
+        <Route path="/home" component={home}/>
     </Router>, document.body);
 
 document.readyState == "loading" ?
---
+++ client/pages/home.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "preact/hooks";
+import { SERVER } from "../../env";
+import { route } from "preact-router";
+
+export default ()=>{
+    const [info, setInfo] = useState<Error|{
+        avatar_url: string,
+        name: string
+    }>();
+    useEffect(()=>{
+        const jwt = localStorage.getItem("token");
+        if(!jwt) route("/");
+        fetch(new URL("/user/info", SERVER), {
+            headers: {
+                Authorization: `Bearer ${jwt}`
+            }
+        }).then(async(e)=>{
+            if(e.ok){
+                setInfo(await e.json());
+            }else{
+                setInfo(new Error());
+            }
+        }).catch(e=>e instanceof Error ? setInfo(e) : setInfo(new Error(`${e}`, { cause: e })))
+    }, []);
+    if(!info) return <h1>読込中...</h1>;
+    if(info instanceof Error){
+        console.error(info);
+        return <>
+            <h1>エラー: {info.name}</h1>
+            {info.stack ?? info.message}
+        </>;
+    }
+    return <div class="home">
+        <img src={info.avatar_url} alt={info.name} />
+        <h1>ようこそ {info.name}さん</h1>
+    </div>
+}
