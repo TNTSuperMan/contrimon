b6178e30ca18ab9d3c73f76c9060b22491eeda48
TNTSuperMan <abworld@outlook.jp>
2025-04-13 16:33:46 +0900

    create user api

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import app from "./app";
 import "./oauth";
+import "./user";
 
 export default app;
---
+++ server/user.ts
@@ -0,0 +1,13 @@
+import { HTTPException } from "hono/http-exception";
+import app from "./app";
+
+app.get("/user/info", async c=>{
+    const { token } = c.get("jwtPayload");
+    console.log(token);
+    const res = await fetch(
+        "https://api.github.com/user", {
+        headers: { Authorization: `Bearer ${token}` }
+    });
+    if(!res.ok) throw new HTTPException(400, { message: "Failed to get user" });
+    return c.json(await res.json());
+});
