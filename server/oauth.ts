90e17591df6081a06c7b8f021f3c0540f9ca5a1e
TNTSuperMan <abworld@outlook.jp>
2025-04-13 15:19:02 +0900

    create oauth api

---
+++ server/oauth.ts
@@ -0,0 +1,25 @@
+import { HTTPException } from "hono/http-exception";
+import app from "./app";
+
+app.get("/oauth/:code", async c=>{
+    const { code } = c.req.param();
+    if(!code) throw new HTTPException(400, { message: "code not specified" });
+    const res = await fetch(
+        "https://github.com/login/oauth/access_token", {
+            body: JSON.stringify({ code,
+                client_id: c.env.CLIENT_ID,
+                client_secret: c.env.CLIENT_SECRET,
+                redirect_uri: `${c.env.CLIENT}/home`
+            }),
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json"
+            }
+        }
+    );
+    if(!res.ok) throw new HTTPException(400, { message: "code invaild" });
+    const { access_token } = await res.json();
+    c.set("jwtPayload", access_token);
+    
+    return c.redirect(`${c.env.CLIENT}/home`);
+});
