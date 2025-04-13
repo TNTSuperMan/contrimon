c2597d380938661be664b2f38b9c6e047c4cdea2
TNTSuperMan <abworld@outlook.jp>
2025-04-13 15:17:47 +0900

    add hono variables type

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
-import { jwt } from "hono/jwt";
+import { jwt, type JwtVariables } from "hono/jwt";
 
 const app = new Hono<{ Bindings: {
     CLIENT_ID: string,
     CLIENT_SECRET: string,
     CLIENT: string,
     SECRET: string
-}}>();
+}, Variables: JwtVariables<string>}>();
 
 app.use("*", async(c,n) => 
     c.req.path.startsWith("/oauth/") ?
