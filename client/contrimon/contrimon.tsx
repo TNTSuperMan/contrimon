c03d4fbc21f6cc9c11756336675deee0cd584592
TNTSuperMan <abworld@outlook.jp>
2025-04-14 21:24:05 +0900

    create face and message data

--- client/contrimon/contrimon.tsx
+++ client/contrimon/contrimon.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "preact/hooks"
 import type { ContributesState } from "../pages/home";
+import { data } from "./data";
 
 export const Contrimon = ({contributes}: {contributes: Error | ContributesState | void}) => {
     const [face, setFace] = useState("-_-");
@@ -10,6 +11,21 @@ export const Contrimon = ({contributes}: {contributes: Error | ContributesState 
             setFace("XoX");
             setMsg(`contributesの取得でエラー発生。${contributes.name}: ${contributes.message}`);
             console.error(contributes);
+        }else if(contributes){
+            const {data:{user:{contributionsCollection:{
+                contributionCalendar:{ totalContributions: total },
+                totalCommitContributions: commit,
+                totalIssueContributions: issue,
+                totalPullRequestContributions: pull,
+                totalPullRequestReviewContributions: review,
+                totalRepositoryContributions: repo,
+            }}}} = contributes;
+            data.forEach(([t,c,i,p,r,f,m])=>{
+                if(t<=total && c<=commit && i<=issue && p<=pull && r<=review){
+                    setFace(f);
+                    setMsg(m);
+                }
+            })
         }
     }, [contributes]);
 
---
+++ client/contrimon/data.ts
@@ -0,0 +1,15 @@
+export const data: [
+    number, //Total
+    number, //Commit
+    number, //Issue
+    number, //Pull
+    number, //Repo
+    string, //Face
+    string, //Msg
+][] = [
+    [0, 0, 0, 0, 0, "._.", "contributionがないなあ..."],
+    [1, 0, 0, 0, 0, "-v-", "生き返ったお"],
+    [5, 0, 0, 0, 0, "^v^", "push push!"],
+    [10, 0, 0, 0, 0, "・o・", "頑張ってるね～"],
+    [20, 0, 0, 0, 0, ">ω<", "すごい！"],
+];
