df9a8ba605035e8abeb56557449d5c3f6c3b3e52
TNTSuperMan <abworld@outlook.jp>
2025-04-21 01:03:53 +0900

    add key getter method

--- server/utils/token.ts
+++ server/utils/token.ts
@@ -3,6 +3,12 @@ import { sign } from "hono/jwt";
 import { getEnv } from "./env";
 import { AES } from "crypto-js";
 
+const key = (c: Context) =>
+    new Uint8Array(
+        getEnv(c, "ENC_SECRET").split("")
+        .reduce<string[]>(((v,c,i)=>(i%2?v[v.length-1]+=c:v.push(c),v)),[])
+        .map(e=>parseInt(e,16)));
+
 export const encodeToken = (c: Context, token: string) =>
     sign({
         token: AES.encrypt(token, getEnv(c, "ENC_SECRET")).toString()
