989d9017658489dfbb8422ccf1edf3d429aadd8d
TNTSuperMan <abworld@outlook.jp>
2025-04-21 12:01:49 +0900

    update Context type(add generic)

--- server/utils/token.ts
+++ server/utils/token.ts
@@ -1,19 +1,20 @@
 import type { Context } from "hono";
 import { sign } from "hono/jwt";
 import { ModeOfOperation, utils } from "aes-js";
+import type { HonoEnv } from "../app";
 
-const key = (c: Context) =>
+const key = (c: Context<HonoEnv>) =>
     new Uint8Array(
-        (c.env.ENC_SECRET as string).split("")
+        c.env.ENC_SECRET.split("")
         .reduce<string[]>(((v,c,i)=>(i%2?v[v.length-1]+=c:v.push(c),v)),[])
         .map(e=>parseInt(e,16)));
 
-export const encodeToken = (c: Context, token: string) =>
+export const encodeToken = (c: Context<HonoEnv>, token: string) =>
     sign({
         token: utils.hex.fromBytes(new ModeOfOperation.ctr(key(c)).encrypt(utils.utf8.toBytes(token)))
     }, c.env.SECRET);
     
-export const getToken = (c: Context) =>
+export const getToken = (c: Context<HonoEnv>) =>
     utils.utf8.fromBytes(
         new ModeOfOperation.ctr(key(c)).decrypt(
             utils.hex.toBytes(c.get("jwtPayload").token)
